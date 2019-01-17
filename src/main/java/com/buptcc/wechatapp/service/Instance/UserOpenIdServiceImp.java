c72e5db1519a05f563b80c2d7c0604ed71fcd606
Heanzy <289139701@qq.com>
2019-01-17 17:03:57 +0800

    UserOpenIdService and controller

---
+++ src/main/java/com/buptcc/wechatapp/service/Instance/UserOpenIdServiceImp.java
@@ -0,0 +1,12 @@
+package com.buptcc.wechatapp.service.Instance;
+
+import com.buptcc.wechatapp.service.UserOpenIdService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserOpenIdServiceImp implements UserOpenIdService {
+    @Override
+    public String getUserOpenId(String code) {
+        return null;
+    }
+}
--- src/main/java/com/buptcc/wechatapp/web/CreatePatternController.java
+++ src/main/java/com/buptcc/wechatapp/web/CreatePatternController.java
@@ -3,8 +3,9 @@ package com.buptcc.wechatapp.web;
 import com.buptcc.wechatapp.service.CreateBagPatternService;
 import com.buptcc.wechatapp.service.CreateCarpetPatternService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
+@Controller
 public class CreatePatternController {
     @Autowired
     private CreateCarpetPatternService createCarpetPatternService;
@@ -17,5 +18,9 @@ public class CreatePatternController {
                     @RequestParam("pName") String pName,
                     @RequestParam("cName") String cName){
         return createBagPatternService.createBagPattern(openId,pName,cName);
+    }
+    @RequestMapping("")
+    public String test(){
+        return "hello worldd";
     }
 }
