31fe9f6e5856998b475f182094d85ab924cd0243
Heanzy <289139701@qq.com>
2019-01-19 11:52:06 +0800

    修改前端访问图片资源路径

--- src/main/java/com/buptcc/wechatapp/config/ImageURLPatternConfiguration.java
+++ src/main/java/com/buptcc/wechatapp/config/ImageURLPatternConfiguration.java
@@ -12,8 +12,7 @@ public class ImageURLPatternConfiguration extends WebMvcConfigurationSupport {
         // location
         // registry.addResourceHandler("/user-images/**").addResourceLocations("file:D:\\app-server\\user-images\\");
         // remote
-        registry.addResourceHandler("/user-images/**").addResourceLocations("file:/home/app-server/user-images/");
-        registry.addResourceHandler("/order-images/**").addResourceLocations("file:/home/app-server/order-images/");
+        registry.addResourceHandler("/image_resources/**").addResourceLocations("/root/env/vrwbg/mini/data");
         super.addResourceHandlers(registry);
     }
 
