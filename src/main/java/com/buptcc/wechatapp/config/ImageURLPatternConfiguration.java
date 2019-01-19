fe78bcdb9e4469669eac02546096a40ff02c5e8c
Heanzy <289139701@qq.com>
2019-01-19 15:26:44 +0800

    "file"

--- src/main/java/com/buptcc/wechatapp/config/ImageURLPatternConfiguration.java
+++ src/main/java/com/buptcc/wechatapp/config/ImageURLPatternConfiguration.java
@@ -11,9 +11,9 @@ public class ImageURLPatternConfiguration extends WebMvcConfigurationSupport {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // location
         // registry.addResourceHandler("/user-images/**").addResourceLocations("file:D:\\app-server\\user-images\\");
-        // remote
-        registry.addResourceHandler("mini/image_restults/**").addResourceLocations("/root/env/vrwbg/mini/result/");
-        registry.addResourceHandler("mini/image_resources/**").addResourceLocations("/root/env/vrwbg/mini/data/");
+        // remoterestults
+        registry.addResourceHandler("mini/image_results/**").addResourceLocations("file:/root/env/vrwbg/mini/result/");
+        registry.addResourceHandler("mini/image_resources/**").addResourceLocations("file:/root/env/vrwbg/mini/data/");
         super.addResourceHandlers(registry);
     }
 
